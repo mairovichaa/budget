ece1dc1c4c7124d14d03eea5a9cc74056885e2e6
Andrei <mairovichaa@yandex.ru>
2019-12-07 16:12:57 +0300

    Implement automatic generation of expense data basing on xlsx

--- app.js
+++ app.js
@@ -5,6 +5,41 @@ const PATH_TO_FILE = "E:\\YandexDisk\\расходы.xlsx";
 
 const PATH_TO_TEMP = "E:\\projects\\budget-node\\temp";
 const TEMP_FILE_NAME = "temp.xlsx";
+const TEMP_FILE = `${PATH_TO_TEMP}\\${TEMP_FILE_NAME}`;
+
+console.log('Start copying of expense file');
+fs.copyFileSync(PATH_TO_FILE, TEMP_FILE);
+console.log('End copying of expense file');
+
+const XLSX = require('xlsx');
+const workbook = XLSX.readFile(TEMP_FILE);
+console.log('Temp file was read');
+
+const worksheet = workbook.Sheets['Расходы'];
+const expenses = XLSX.utils.sheet_to_json(worksheet);
+
+function getFormattedDate(expense) {
+    const date = XLSX.SSF.parse_date_code(expense['дата']);
+    const day = ("0" + date.d).slice(-2);
+    const month = ("0" + date.m).slice(-2);
+    const year = date.y;
+    return `${day}.${month}.${year}`;
+}
+
+const outExpense = [];
 
 
-fs.copyFileSync(PATH_TO_FILE, `${PATH_TO_TEMP}\\${TEMP_FILE_NAME}`);
+expenses.forEach(e => {
+    const outE = {
+        date: getFormattedDate(e),
+        sum: e['сумма'],
+        category: e['категория']
+    };
+    outExpense.push(outE);
+});
+
+const data = `\nlet expenses = ${JSON.stringify(outExpense)};`;
+
+fs.writeFileSync('E:\\projects\\budget\\src\\main\\resources\\data.js', data);
+
+fs.unlinkSync(TEMP_FILE);
