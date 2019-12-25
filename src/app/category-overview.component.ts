c7a52d246770545b32f25c4a28ba9fc40d38aaca
amairovi <andrei.mairovich@electrolux.com>
2019-12-25 15:26:24 +0300

    Create a separate component for category overview

---
+++ src/app/category-overview.component.ts
@@ -0,0 +1,47 @@
+import {Component} from '@angular/core';
+import {ExpenseService} from './expense.service';
+import * as _ from "lodash";
+
+@Component({
+    selector: 'category-overview',
+    template: `
+        <div style="width: 600px; float: left; margin-left: 20px" class="card">
+            <div class="card-body">
+                <h5 class="card-title">Category overview</h5>
+                <div>
+                    <p>Total: {{total | number:'0.2-2'}}</p>
+                    <p>Annual average: {{annual | number:'0.2-2'}}</p>
+                </div>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Category</th>
+                        <th scope="col">Total</th>
+                        <th scope="col">Annual Avg.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr *ngFor="let entry of data">
+                        <td>{{entry.category}}</td>
+                        <td>{{entry.total | number:'0.2-2'}}</td>
+                        <td>{{entry.annual | number:'0.2-2'}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    `,
+    styles: []
+})
+export class CategoryOverviewComponent {
+    data = [];
+    total = 0;
+    annual = 0;
+
+    constructor(private expenseService: ExpenseService) {
+        this.data = expenseService.getOverview();
+        this.total = _(this.data).sumBy(e => e.total);
+        this.annual = this.total / ExpenseService.AMOUNT_OF_YEARS;
+    }
+
+}
