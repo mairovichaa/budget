998d722602e2cb92fd3a16e09f4b3f6e22705970
Andrei <mairovichaa@yandex.ru>
2020-01-06 21:04:03 +0300

    Add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,22 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
-        <router-outlet></router-outlet>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" routerLink="/">Budget</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item" routerLinkActive="active">
+                        <a class="nav-link" routerLink="/date">Date</a>
+                    </li>
+                    <li class="nav-item" routerLinkActive="active">
+                        <a class="nav-link" routerLink="/category">Category</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div style="margin-top: 10px">
+            <router-outlet></router-outlet>
+        </div>
     `
 })
 export class AppComponent {
