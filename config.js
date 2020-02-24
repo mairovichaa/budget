e12e0c5beaa32c74e71105419d9d5beb582c18a7
Andrei <mairovichaa@yandex.ru>
2020-02-24 11:16:31 +0300

    Remove script for generation of static data

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PATH_TO_SOURCE_FILE: 'path to xlsx file with expenses',
-    PATH_TO_TEMP_DIR: 'dir for temporal persistence',
-    TEMP_FILE_NAME: 'name of the file for temporal persistence of source xlsx file copy',
-    PATH_TO_DATA_FILE: 'full path to the file where data has to be saved'
-};
